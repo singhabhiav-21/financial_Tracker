3d310b245450a13aef3e032f745d85966d41dcdb
Abhinav Singh <as228wi@student.lnu.se>
2025-09-06 20:26:45 +0200

    First Commit- Database

---
+++ budget_db.sql
@@ -0,0 +1,48 @@
+
+
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name    VARCHAR(50) NOT NULL ,
+    email   VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    profile_photo VARCHAR(255),
+    date_added DATETIME DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS account(
+    account_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    account_name VARCHAR(50) NOT NULL,
+    account_type VARCHAR(50) NOT NULL,
+    account_balance DECIMAL(12,2),
+    currency VARCHAR(10) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    foreign key (user_id) references users(user_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS category(
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR (50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_on DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS transactions(
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    amount DECIMAL(12,2) NOT NULL,
+    description VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) references users(user_id),
+    FOREIGN KEY (category_id) references category(category_id)
+);
+
+
+
