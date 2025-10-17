7c78e2d66d8fa719995d0cdc82b8ea09e0e55280
Abhinav Singh <abhinav.singh9421@gmail.com>
2025-10-17 23:55:00 +0200

    Exchange rate function

--- graphs/ExchangeRates.py
+++ graphs/ExchangeRates.py
@@ -1,19 +1,36 @@
 import requests
-from dotenv import load_dotenv
 import os
 
-
-load_dotenv()
-API_KEY = os.getenv("EXCHANGE_API_KEY")
-
-url = f"https://api.exchangerate-api.com/v4/latest/SEK"
+url = f"https://api.exchangerate-api.com/v4/latest/USD"
 response = requests.get(url, timeout=10)
 data = response.json()
+base_currency = data['base']
 
 rates = {
-    'SEK': 1.0,  # USD is always 1 relative to itself
-    'USD': data['rates']['USD'],
-    'INR': data['rates']['INR']
+    base_currency: 1.0,
+    'SEK': data['rates'].get('SEK'),
+    'INR': data['rates'].get('INR')
 }
 
-print(rates)
+
+def add_exchangeRate(currency1, currency2):
+    new_url = f"{url.rsplit('/', 1)[0]}/{currency1}"
+    try:
+        response = requests.get(new_url, timeout=10)
+        data = response.json()
+
+        base = data['base']
+        rates[base] = 1.0
+
+        if currency2 in data['rates']:
+            rates[currency2] = data['rates'][currency2]
+            return data['rates'][currency2]
+        else:
+            print(f"Currency {currency2} not found!")
+            return False
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+
+print(add_exchangeRate("SEK", "JPY"))
