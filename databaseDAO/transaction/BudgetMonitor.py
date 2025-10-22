16d2e5b5b54000f478f148b2f2e3b8e84b697690
Abhinav Singh <abhinav.singh9421@gmail.com>
2025-10-22 23:11:10 +0200

    get budget function

--- databaseDAO/transaction/BudgetMonitor.py
+++ databaseDAO/transaction/BudgetMonitor.py
@@ -2,7 +2,20 @@ from financial_Tracker.databaseDAO.sqlConnector import get_connection
 import pandas as pd
 
 conn = get_connection()
-corsor = conn.cursor()
+cursor = conn.cursor()
 
-def set_budget(user_id, budget):
-    query = "SELECT"
+
+def get_budget(user_id):
+    query = """
+    SELECT budget_id 
+    FROM budget
+    WHERE user_id = %s
+    """
+    cursor.execute(query, user_id,)
+    result = cursor.fetchone()
+    if not result:
+        print("The user has no budget selected!")
+    return result
+
+
+def set_budget(budget_id, amount, month, year, repeat:bool):
