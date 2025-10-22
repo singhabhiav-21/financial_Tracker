c53cfb65e6c3af02bb31f1448ca7416ae3f298f7
Abhinav Singh <abhinav.singh9421@gmail.com>
2025-10-22 23:44:18 +0200

    function to set up a budget for a month

--- databaseDAO/transaction/BudgetMonitor.py
+++ databaseDAO/transaction/BudgetMonitor.py
@@ -18,4 +18,27 @@ def get_budget(user_id):
     return result
 
 
-def set_budget(budget_id, amount, month, year, repeat:bool):
+def set_budget(user_id, category_id, amount, month, year, repeat: bool):
+    query = """
+    INSERT INTO budget(category_id, amount, month, year) VALUES (%s,%s,%s,%s)
+    WHERE budget_id = %s
+    """
+    if amount <= 0:
+        print("the amount cannot be less than or equal to 0")
+        return False
+    if month > 12 and month < 1:
+        print("The month cannot be less than 1 or greater than 12")
+        return False
+    budget_id = get_budget(user_id)
+    cursor.execute(query, (category_id, amount, month, year, budget_id))
+    if repeat:
+        repeat_budget(budget_id, category_id, amount)
+    else:
+        pass
+    return True
+
+
+def repeat_budget(budget_id, category_id, amount):
+
+
+
