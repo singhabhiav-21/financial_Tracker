6231d73802b98351e2bb031eee7c0581fcb778e0
Abhinav Singh <abhinav.singh9421@gmail.com>
2026-06-30 15:41:10 +0200

    Added- Tables of users and trancasction for tests

--- databaseDAO/test/test_user_dao.py
+++ databaseDAO/test/test_user_dao.py
@@ -2,6 +2,7 @@ import unittest
 from databaseDAO.userDAO import register, logIn, update_userinfo, update_password
 from databaseDAO.sqlConnector import get_connection
 
+
 class TestUserManagement(unittest.TestCase):
 
     @classmethod
@@ -10,6 +11,32 @@ class TestUserManagement(unittest.TestCase):
         cls.conn = get_connection()
         cls.cursor = cls.conn.cursor()
         # Optionally, clear the test users table
+        cls.cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+            user_id INT PRIMARY KEY AUTO_INCREMENT,
+            name    VARCHAR(50) NOT NULL ,
+            email   VARCHAR(100) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            profile_photo VARCHAR(255) NUll,
+            date_added DATETIME DEFAULT current_timestamp
+        ); """)
+
+        cls.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS transactions(
+            transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+            user_id INT NOT NULL,
+            category_id INT NOT NULL,
+            name VARCHAR(100) NOT NULL,
+            amount DECIMAL(12,2) NOT NULL,
+            description VARCHAR(255),
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            transaction_date DATE,
+            balance decimal(15,2),
+            transaction_hash VARCHAR(250) NULL,
+            CONSTRAINT uniq_user_tx UNIQUE (user_id, transaction_hash),
+            FOREIGN KEY (user_id) references users(user_id),
+            FOREIGN KEY (category_id) references category(category_id)
+            );""")
+        cls.conn.commit()
         cls.cursor.execute(
             "DELETE FROM transactions WHERE user_id IN (SELECT user_id FROM users WHERE email LIKE '%example.com')"
         )
